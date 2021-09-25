bd22ba0ecae75c2130862b771a0be72302271733
tompng <tomoyapenguin@gmail.com>
2021-09-25 19:59:31 +0900

    imogai complete

--- imogai.js
+++ imogai.js
@@ -1,15 +1,25 @@
+function mod1(a){
+  a - Math.floor(a) // note: a%1 != mod1(a) for a<0
+}
+function imogaiPatternFunc(z,d,r) {
+  const modz1 = z - Math.floor(z) // note: z % 1 != mod(z, 1) for z < 0
+  return r*r*((d-1)**12+16*(modz1-1/2)**2-1)**2-4+(z>4 ? 32 : 0)+2*Math.exp(-d-6*modz1+4)
+}
 function imogaiFunc(x, y, z) {
-  x*=2.2
-  y*=2.2
-  z*=2.2
-  z+=4
-  const L = Math.hypot(x,y,z)
-  const X=2*Math.acos(z/L)
-  const Z=2*Math.log(L)+X
-  const th = Math.atan2(y,x)
-  const a = ((X-1)**8+16*((Z-th/Math.PI/4)%1-1/2)**2-1)**2*L*L-16+(Z-th/Math.PI/4>4 ? 32 : 0)
-  const b = ((X-1)**8+16*((Z-th/Math.PI/4 - 1 / 2)%1-1/2)**2-1)**2*L*L-16+
-    (Z-th/Math.PI/4-1/2>4 ? 32 : 0)+
-    (Z-th/Math.PI/4-1/2>3 ? Math.max(32*(th-Math.sin(X)), 0) : 0)
-  return Math.min(a,b)
+  x*=3
+  y*=3
+  z*=3
+  x+=0.001
+  y-=4
+  const r = Math.hypot(x,y,z)
+  const D = 2*Math.acos(-y/r)
+  const Z = 2*Math.log(r)+D
+  const th = Math.PI/2*(Math.abs(z)/z||1)-Math.atan(x/z) // Math.arctan2(z,x)
+  const za = Z-th/4/Math.PI
+  const zb = za-1/2
+  return Math.min(
+    imogaiPatternFunc(za,D,r),
+    imogaiPatternFunc(zb,D,r) + 64 * (zb>3 ? Math.max(th-Math.sin(D), 0) : 0),
+    (32-24*Math.sin(4*Math.PI*za))*(x*x+z*z)+((y+4)/3)**8 - 1
+  )
 }
